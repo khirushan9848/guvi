30ed5504efaf04f22a907d9530832347987bf0d8
khirushan9848 <39775212+khirushan9848@users.noreply.github.com>
2018-08-01 11:55:38 +0530

    Create interev.java

---
+++ interev.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class guvi21
+{
+    public static void main(String ar4sg[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.next();
+        char c[]=s.toCharArray();
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            System.out.print(c[i]);
+        }
+    }
+}
