bba50e9aff3adf9cae1e61afeed25e6ec3bd87c0
khirushan9848 <39775212+khirushan9848@users.noreply.github.com>
2018-08-01 12:00:27 +0530

    Create dotadd.java

---
+++ dotadd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class guvi23
+{
+    public static void main(String ar4sg[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String n=sc.next();
+        System.out.print(n+".");
+    }
+}
