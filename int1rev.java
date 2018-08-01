014290fc71cf2897059a526f1640ccfa74961ec6
khirushan9848 <39775212+khirushan9848@users.noreply.github.com>
2018-08-01 11:57:31 +0530

    Create int1rev.java

---
+++ int1rev.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class guvi21
+{
+    public static void main(String ar4sg[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String n=sc.next();
+        char arr[]=n.toCharArray();
+        for(int i=n.length()-1;i>=0;i--)
+        {
+            System.out.print(arr[i]);
+        }
+    }
+}
